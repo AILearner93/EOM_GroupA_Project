fa42e944b7a5ce6a35d482bfb19305424c18a6fe
Taylan <taylanan@live.com>
2023-09-28 21:47:25 +0100

    Deleted server file

--- server/app.py
+++ server/app.py
@@ -8,16 +8,17 @@ def runServer():
     try:
         load_dotenv()
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #added time out of 20 seconds
+        server.settimeout(20)
         host = os.getenv('HOST')
         port = int(os.getenv("PORT"))
-
+        print(port)
         server.bind((host, port))
         server.listen(5)
         print("Server is running")
         while True:
             connection, _ = server.accept()
-            # 2^16 = 65536
-            request = connection.recv(65536).decode('utf-8')
+            request = connection.recv(1024).decode('utf-8')
             request = eval(request)
             event = request['event']
             data = request['data']
@@ -28,11 +29,19 @@ def runServer():
             handle_requests(event, connection, data)
             connection.close()
 
+    except KeyboardInterrupt:
+        #Thiss allows ctrl+c to exit server
+        print("\nShutting down the server.")
+
     except Exception as e:
         print(e)
         print("Server is not running")
 
+    finally:
+        server.close()
+
 
 
 if __name__ == '__main__':
+    print("server is running")
     runServer()
